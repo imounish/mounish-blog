f9969b2ec4021c26535607de9a2a865920d4f3ca
Mounish Pedagandham <mounish@Mounishs-MacBook-Pro.local>
2023-10-17 20:04:56 +0530

    implemented tags page

--- src/components/tags/TagsArray.jsx
+++ src/components/tags/TagsArray.jsx
@@ -7,7 +7,7 @@ function TagsArray({ tags, className }) {
       {tags.map((tag) => (
         <li key={tag.title}>
           <Link
-            to={`/tags/${tag.slug.current}`}
+            to={`/tags/?search=${tag.slug.current}`}
             className=" font-worksans font-light uppercase text-sm rounded-md bg-gray-700 text-gray-200 hover:bg-gray-600 dark:bg-gray-300 dark:text-gray-800 dark:hover:bg-gray-200 py-1.5 px-2"
           >
             {tag.title}
